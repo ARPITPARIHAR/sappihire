b599d8bfc7d45085108bb93a760cc4a0cb3c5721
MLA Technical <mlatechnical2427@gmail.com>
2024-07-19 18:54:15 +0530

    :WID url correction

--- routes/web.php
+++ routes/web.php
@@ -26,12 +26,11 @@ Route::controller(PageController::class)->group(function () {
     Route::get('infastructure', 'infastructure')->name('infastructure');
     Route::get('contact-us', 'contact_us')->name('contact-us');
     Route::get('gallery', 'gallery')->name('gallery');
-    Route::get('placementservice', 'placementservice')->name('placementservice');
-    Route::get('boardofdirectory', 'boardofdirectory')->name('boardofdirectory');
-    Route::get('teammember', 'teamofmember')->name('teamofmember');
-    Route::get('traningprogramme', 'traningprogramme')->name('traningprogramme');
+    Route::get('placement-service', 'placementservice')->name('placementservice');
+    Route::get('board-of-directory', 'boardofdirectory')->name('boardofdirectory');
+    Route::get('team-member', 'teamofmember')->name('teamofmember');
+    Route::get('traning-programme', 'traningprogramme')->name('traningprogramme');
     Route::get('login-register', 'login_register')->name('login-register');
-
 });
 
 Route::get('logout', [LoginController::class, 'logout'])->name('logout');
@@ -45,6 +44,3 @@ Route::post('/register', [LoginController::class, 'register'])->name('register')
 Route::get('/training-events', [EventController::class, 'index'])->name('training.search');
 
 Route::post('/contact/store', [ContactController::class, 'store'])->name('contact.store');
-
-
-
