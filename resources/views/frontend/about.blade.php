48c57785223c52b3577619e9814cad5de5ae9618
Arpit Parihar <parihararpiy@gmail.com>
2024-07-22 12:02:46 +0530

    initial commit

--- resources/views/frontend/about.blade.php
+++ resources/views/frontend/about.blade.php
@@ -3,14 +3,21 @@
 
 @include('frontend.includes.navbar')
 
+<section class="about">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12">
+				<div class="head">
+					<h2>About us</h2>
+                    @foreach (\App\Models\About::all() as $aboutService)
+                    <p>{{ $aboutService->brief_description }}</p>
+                @endforeach
+				</div>
+			</div>
 
-<div class="abt_instut">
-    <h2>About Institute</h2>
-    @foreach (\App\Models\About::all() as $aboutService)
-    <p>{{ $aboutService->brief_description }}</p>
-@endforeach
-
-</div>
+		</div>
+	</div>
+</section>
 
 @include('frontend.includes.footer')
 @section('style')
--- resources/views/frontend/hostel.blade.php
+++ resources/views/frontend/hostel.blade.php
@@ -1,21 +1,25 @@
-
 @extends('frontend.layouts.app')
 @section('meta_title','rcem')
 
 @include('frontend.includes.navbar')
 
-<div class="abt_instut">
-    <h2>Hostel Facility</h2>
-    @foreach (\App\Models\Hostelservice::all() as $hostelService)
-    <p>{{ $hostelService->brief_description }}</p>
-@endforeach
+<section class="about">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12">
+				<div class="head">
+					<h2>Hostel Facility</h2>
+                    @foreach (\App\Models\Hostelservice::all() as $hostelService)
+                    <p>{{ $hostelService->brief_description }}</p>
+                @endforeach
+				</div>
+			</div>
 
-</div>
+		</div>
+	</div>
+</section>
+
 @include('frontend.includes.footer')
-
-
-
-
 @section('style')
 
 @endsection
