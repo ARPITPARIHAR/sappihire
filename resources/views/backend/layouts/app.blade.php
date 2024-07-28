c078f6766c3c349e6573b7ea74115a70f9bfe2c1
M.L. Prajapat <mangilalprajapat3@gmail.com>
2024-07-28 19:40:03 +0530

    :WID Admin Meta Title Added

--- resources/views/backend/layouts/app.blade.php
+++ resources/views/backend/layouts/app.blade.php
@@ -6,6 +6,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>@yield('meta_title') | {{ env('APP_NAME') }}</title>
     <link rel="icon" href="{{ asset('backend/assets/images/favicon.ico') }}" type="image/x-icon">
     <!-- Google font-->
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600" rel="stylesheet">
